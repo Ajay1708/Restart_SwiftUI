e4e9c8f24afc897395b012522023e7ff06134360
Venkataparas <venkata@parastechnologies.com>
2022-03-13 08:59:12 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Venkata Ajay Sai Nellori on 13/03/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
